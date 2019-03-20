e201fed0f52718574353b1466a768dc7d62ecdf7
Kairui <mankairui@gmail.com>
2019-03-20 15:09:58 -0700

    add reset button

--- guessGame.js
+++ guessGame.js
@@ -4,13 +4,25 @@ var pickedcolor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click",function () {
+        colors = generateColors(6);
+        pickedcolor = pickColor();
+        colorDisplay.textContent = pickedcolor;
+    for (var i = 0; i< squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor= "gray";
 
+
+})
 for (var i = 0; i< squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click",function () {
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedcolor){
             message.textContent = "Correct!";
+            resetBtn.textContent = "Play again?"
             for(var i=0; i < squares.length; i++ ){
                 squares[i].style.backgroundColor = clickedColor;
 
