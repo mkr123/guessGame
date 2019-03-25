f48386a7847ec6624331518185eb2621662d93f7
Kairui <mankairui@gmail.com>
2019-03-25 14:48:06 -0700

    style change

--- guessGame.js
+++ guessGame.js
@@ -1,18 +1,56 @@
-var colors = generateColors(6);
+var numofsquares = 6;
+var colors = generateColors(numofsquares);
 var squares = document.querySelectorAll(".square");
 var pickedcolor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetBtn = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+easyBtn.addEventListener("click",function () {
+    numofsquares = 3;
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    colors = generateColors(numofsquares);
+    pickedcolor = pickColor();
+    colorDisplay.textContent= pickedcolor;
+    for(var i =0;i<squares.length;i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor=colors[i];
+        }
+        else{
+            squares[i].style.display= "none";
+        }
+    }
+});
+
+
+hardBtn.addEventListener("click",function () {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    colors = generateColors(numofsquares);
+    pickedcolor = pickColor();
+    colorDisplay.textContent= pickedcolor;
+    for(var i =0;i<squares.length;i++){
+        squares[i].style.backgroundColor=colors[i];
+        squares[i].style.display= "block";
+        }
+
+
+});
+
+
+
+
 resetBtn.addEventListener("click",function () {
-        colors = generateColors(6);
+        colors = generateColors(numofsquares);
         pickedcolor = pickColor();
         colorDisplay.textContent = pickedcolor;
     for (var i = 0; i< squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor= "gray";
+    h1.style.background= "mediumspringgreen";
 
 
 })
@@ -27,7 +65,7 @@ for (var i = 0; i< squares.length; i++){
                 squares[i].style.backgroundColor = clickedColor;
 
             }
-            h1.style.backgroundColor= clickedColor;
+            h1.style.background= clickedColor;
         }
         else{
             message.textContent = "Try again."
